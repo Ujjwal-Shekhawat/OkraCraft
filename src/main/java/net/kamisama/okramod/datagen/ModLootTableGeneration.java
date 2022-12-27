8c56be1b5c6516dd7d8df54213a2afb86de0e2e2
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2022-12-28 01:53:46 +0530

    modified veggie knife and added datagen for crop

--- src/main/java/net/kamisama/okramod/datagen/ModLootTableGeneration.java
+++ src/main/java/net/kamisama/okramod/datagen/ModLootTableGeneration.java
@@ -2,20 +2,22 @@ package net.kamisama.okramod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
+import net.kamisama.okramod.Block.ModBlocks;
 import net.kamisama.okramod.Item.ModItems;
-import net.minecraft.client.render.model.json.ModelOverride;
-import net.minecraft.item.Item;
+import net.kamisama.okramod.OkraMod;
+import net.minecraft.data.server.BlockLootTableGenerator;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
-import net.minecraft.loot.condition.LootCondition;
+import net.minecraft.loot.condition.LootConditionManager;
+import net.minecraft.loot.condition.LootConditionType;
+import net.minecraft.loot.condition.LootConditionTypes;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.context.LootContextTypes;
-import net.minecraft.loot.entry.AlternativeEntry;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.ConditionalLootFunction;
-import net.minecraft.loot.function.LootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 
 import java.util.function.BiConsumer;
@@ -29,5 +31,9 @@ public class ModLootTableGeneration extends SimpleFabricLootTableProvider {
     public void accept(BiConsumer<Identifier, LootTable.Builder> identifierBuilderBiConsumer) {
         identifierBuilderBiConsumer.accept(new Identifier("minecraft", "blocks/grass"),
                 LootTable.builder().pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F)).with(ItemEntry.builder(ModItems.OKRA_SEEDS).conditionally(RandomChanceLootCondition.builder(0.125f)).alternatively(ItemEntry.builder(Items.WHEAT_SEEDS).conditionally(RandomChanceLootCondition.builder(0.125f))))));
+
+        identifierBuilderBiConsumer.accept(new Identifier(OkraMod.MOD_ID, "blocks/okra_crop"),
+                BlockLootTableGenerator.cropDrops(ModBlocks.OKRA_CROP, ModItems.OKRA, ModItems.OKRA_SEEDS,
+                        RandomChanceLootCondition.builder(1f)));
     }
 }
